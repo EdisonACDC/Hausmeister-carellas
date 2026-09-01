9fbf85a99eaec2e2a82b1ae5f00f16deb981efba
EdisonACDC <77065985+EdisonACDC@users.noreply.github.com>
2026-09-01 13:38:22 +0200

    Run separate public and admin web interfaces

--- hausmeister_carellas/run.sh
+++ hausmeister_carellas/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/with-contenv bashio
 set -e
-mkdir -p /config/uploads
-exec uvicorn app.main:app --host 0.0.0.0 --port 8099
+mkdir -p /data/uploads
+uvicorn app.main:public_app --host 0.0.0.0 --port 8080 &
+exec uvicorn app.main:admin_app --host 0.0.0.0 --port 8099
