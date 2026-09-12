631d721bbcc3f10d40a354a994630c90ccd8bd4f
EdisonACDC <77065985+EdisonACDC@users.noreply.github.com>
2026-09-12 12:06:57 +0200

    Start Home Assistant sensor publisher in beta

--- hausmeister_carellas_beta/run.sh
+++ hausmeister_carellas_beta/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
 set -e
 mkdir -p /data/uploads
+python3 -m app.ha_sensor_publisher &
 uvicorn app.main:public_app --host 0.0.0.0 --port 8080 &
 exec uvicorn app.main:admin_app --host 0.0.0.0 --port 8099
